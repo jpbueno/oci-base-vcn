3591e44eb75fcae7c25b7516bf93baabe978f0d3
JP Santana <jp.santana@oracle.com>
2025-01-05 10:19:18 -0300

    new vcn version

--- security.tf
+++ security.tf
@@ -1,266 +1,73 @@
 resource oci_core_security_list k8s_api_security_list {
   compartment_id = var.compartment_ocid
-  display_name = "k8s_api_security_list"
+  display_name   = "k8s_api_security_list"
+
   egress_security_rules {
-    description      = "Allow Kubernetes Control Plane to communicate with OKE"
-    destination      = "all-iad-services-in-oracle-services-network"
-    destination_type = "SERVICE_CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "443"
-      min = "443"
-    }
-  }
-  egress_security_rules {
-    description      = "All traffic to worker nodes"
-    destination      = "10.0.0.0/16"
+    description      = "Allow all egress traffic"
+    destination      = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
+    protocol         = "all"
+    stateless        = "false"
   }
-  egress_security_rules {
-    description      = "Path discovery"
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    icmp_options {
-      code = "4"
-      type = "3"
-    }
-    protocol  = "1"
-    stateless = "false"
-  }
-  freeform_tags = {
-  }
+
   ingress_security_rules {
     description = "External access to Kubernetes API endpoint"
-    protocol    = "6"
+    protocol    = "6" # TCP
     source      = "0.0.0.0/0"
     source_type = "CIDR_BLOCK"
     stateless   = "false"
     tcp_options {
       max = "6443"
       min = "6443"
-      #source_port_range = <<Optional value not found in discovery>>
-    }
-    #udp_options = <<Optional value not found in discovery>>
-  }
-  ingress_security_rules {
-    description = "Kubernetes worker to Kubernetes API endpoint communication"
-    protocol    = "6"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "6443"
-      min = "6443"
-      #source_port_range = <<Optional value not found in discovery>>
-    }
-    #udp_options = <<Optional value not found in discovery>>
-  }
-  ingress_security_rules {
-    description = "Kubernetes worker to control plane communication"
-    protocol    = "6"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "12250"
-      min = "12250"
     }
   }
-  ingress_security_rules {
-    description = "Path discovery"
-    icmp_options {
-      code = "4"
-      type = "3"
-    }
-    protocol    = "1"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-  }
-  vcn_id = oci_core_vcn.dev-vcn.id
-}
 
-resource oci_core_security_list private_security_list {
-  compartment_id = var.compartment_ocid
-  display_name = "private_security_list"
-  egress_security_rules {
-    description      = "Allow pods on one worker node to communicate with pods on other worker nodes"
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    protocol  = "all"
-    stateless = "false"
-  }
-  egress_security_rules {
-    description      = "Access to Kubernetes API Endpoint"
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "6443"
-      min = "6443"
-    }
-  }
-  egress_security_rules {
-    description      = "Kubernetes worker to control plane communication"
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "12250"
-      min = "12250"
-    }
-  }
-  egress_security_rules {
-    description      = "Path discovery"
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    icmp_options {
-      code = "4"
-      type = "3"
-    }
-    protocol  = "1"
-    stateless = "false"
-  }
-  egress_security_rules {
-    description      = "Allow nodes to communicate with OKE to ensure correct start-up and continued functioning"
-    destination      = "all-iad-services-in-oracle-services-network"
-    destination_type = "SERVICE_CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "443"
-      min = "443"
-    }
-  }
-  egress_security_rules {
-    description      = "ICMP Access from Kubernetes Control Plane"
-    destination      = "0.0.0.0/0"
-    destination_type = "CIDR_BLOCK"
-    icmp_options {
-      code = "4"
-      type = "3"
-    }
-    protocol  = "1"
-    stateless = "false"
-  }
-  egress_security_rules {
-    description      = "Worker Nodes access to Internet"
-    destination      = "0.0.0.0/0"
-    destination_type = "CIDR_BLOCK"
-    protocol  = "all"
-    stateless = "false"
-  }
-  freeform_tags = {
-  }
   ingress_security_rules {
-    description = "Allow pods on one worker node to communicate with pods on other worker nodes"
+    description = "Allow all traffic from 10.0.0.0/16"
     protocol    = "all"
     source      = "10.0.0.0/16"
     source_type = "CIDR_BLOCK"
     stateless   = "false"
   }
+
+  vcn_id = oci_core_vcn.dev-vcn.id
+}
+
+resource oci_core_security_list private_security_list {
+  compartment_id = var.compartment_ocid
+  display_name   = "private_security_list"
+
+  egress_security_rules {
+    description      = "Allow all egress traffic"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+    stateless        = "false"
+  }
+
   ingress_security_rules {
-    description = "Path discovery"
-    icmp_options {
-      code = "4"
-      type = "3"
-    }
-    protocol    = "1"
+    description = "Allow all traffic from 10.0.0.0/16"
+    protocol    = "all"
     source      = "10.0.0.0/16"
     source_type = "CIDR_BLOCK"
     stateless   = "false"
   }
-  ingress_security_rules {
-    description = "TCP access from Kubernetes Control Plane"
-    protocol    = "6"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-  }
-  ingress_security_rules {
-    description = "Inbound SSH traffic to worker nodes"
-    protocol    = "6"
-    source      = "0.0.0.0/0"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "22"
-      min = "22"
-    }
-  }
-  ingress_security_rules {
-    protocol    = "6"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "32026"
-      min = "32026"
-    }
-  }
-  ingress_security_rules {
-    protocol    = "6"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "10256"
-      min = "10256"
-    }
-  }
-  ingress_security_rules {
-    protocol    = "6"
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
-    tcp_options {
-      max = "30882"
-      min = "30882"
-    }
-  }
+
   vcn_id = oci_core_vcn.dev-vcn.id
 }
 
 resource oci_core_security_list lb_security_list {
   compartment_id = var.compartment_ocid
-  display_name = "lb_security_list"
+  display_name   = "lb_security_list"
+
   egress_security_rules {
-    destination      = "10.0.0.0/16"
+    description      = "Allow all egress traffic"
+    destination      = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "32026"
-      min = "32026"
-    }
+    protocol         = "all"
+    stateless        = "false"
   }
-  egress_security_rules {
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "10256"
-      min = "10256"
-    }
-  }
-  egress_security_rules {
-    destination      = "10.0.0.0/16"
-    destination_type = "CIDR_BLOCK"
-    protocol  = "6"
-    stateless = "false"
-    tcp_options {
-      max = "30882"
-      min = "30882"
-    }
-  }
-  freeform_tags = {
-  }
+
   ingress_security_rules {
     protocol    = "6"
     source      = "0.0.0.0/0"
@@ -271,6 +78,7 @@ resource oci_core_security_list lb_security_list {
       min = "9003"
     }
   }
+
   ingress_security_rules {
     protocol    = "6"
     source      = "0.0.0.0/0"
@@ -281,5 +89,26 @@ resource oci_core_security_list lb_security_list {
       min = "9090"
     }
   }
+
+  ingress_security_rules {
+    description = "Allow HTTP traffic from anywhere"
+    protocol    = "6"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+    tcp_options {
+      max = "80"
+      min = "80"
+    }
+  }
+
+  ingress_security_rules {
+    description = "Allow all traffic from 10.0.0.0/16"
+    protocol    = "all"
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+
   vcn_id = oci_core_vcn.dev-vcn.id
 }
